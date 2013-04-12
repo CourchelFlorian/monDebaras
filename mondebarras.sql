92a1f3453c79fd4b33ff87657e88759ea974443e
Courchel <courche.florian@gmail.com>
2013-04-12 13:55:17 +0200

    BDD 12/04/2013

---
+++ mondebarras.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le : Ven 12 Avril 2013 à 11:47
+-- Version du serveur: 5.5.20
+-- Version de PHP: 5.3.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `mondebarras`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membre`
+--
+
+CREATE TABLE IF NOT EXISTS `membre` (
+  `id` int(11) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `mdp` varchar(10) NOT NULL,
+  `droit` int(11) NOT NULL,
+  `mail` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `droit` (`droit`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `objet`
+--
+
+CREATE TABLE IF NOT EXISTS `objet` (
+  `id` int(11) NOT NULL,
+  `idCategorie` int(11) NOT NULL,
+  `idVendeur` int(11) NOT NULL,
+  `prix` double NOT NULL,
+  `prixSeuil` double NOT NULL,
+  `tempsRestant` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idCategorie` (`idCategorie`),
+  KEY `idVendeur` (`idVendeur`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `profil`
+--
+
+CREATE TABLE IF NOT EXISTS `profil` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vente`
+--
+
+CREATE TABLE IF NOT EXISTS `vente` (
+  `id` int(11) NOT NULL,
+  `idObjet` int(11) NOT NULL,
+  `idAcheteur` int(11) NOT NULL,
+  `prix` double NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idObjet` (`idObjet`),
+  KEY `idAcheteur` (`idAcheteur`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `membre`
+--
+ALTER TABLE `membre`
+  ADD CONSTRAINT `membre_ibfk_1` FOREIGN KEY (`droit`) REFERENCES `profil` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `objet`
+--
+ALTER TABLE `objet`
+  ADD CONSTRAINT `objet_ibfk_2` FOREIGN KEY (`idVendeur`) REFERENCES `membre` (`id`),
+  ADD CONSTRAINT `objet_ibfk_1` FOREIGN KEY (`idCategorie`) REFERENCES `categorie` (`id`);
+
+--
+-- Contraintes pour la table `vente`
+--
+ALTER TABLE `vente`
+  ADD CONSTRAINT `vente_ibfk_2` FOREIGN KEY (`idAcheteur`) REFERENCES `membre` (`id`),
+  ADD CONSTRAINT `vente_ibfk_1` FOREIGN KEY (`idObjet`) REFERENCES `objet` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
